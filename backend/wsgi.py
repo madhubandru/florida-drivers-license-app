0bca9d6f79ec7b7698a2a7144462b8513305b703
MADHU BANDRU <mbandru2@dxc.com>
2025-03-22 12:34:47 -0400

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,9 +1,16 @@
 import sys
 import os
 
-# Add the path to your application
-path = os.path.dirname(os.path.abspath(__file__))
-if path not in sys.path:
-    sys.path.append(path)
+# Add your project directory to the sys.path
 
+# # OLD PATH
+# path = os.path.dirname(os.path.abspath(__file__))
+# if path not in sys.path:
+#     sys.path.append(path)
+# This path as per project uploaded in pythonanywhere web app platform
+project_home = '/home/madhubandru/florida-drivers-license-app/backend'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Import your Flask app
 from app import app as application
